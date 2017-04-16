35b0436a2c4a3987a2450cec2a35b04f0334ebac
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-04-16 17:21:34 +0200

    Added a loader to lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -10,15 +10,26 @@ const fHandleLightbox = function(oEvent) {
     sImgSrc = $img.getAttribute("src");
     sLightboxSrc = sImgSrc.substring(0, sImgSrc.indexOf("."))+ "-full.jpg";
     sCaption = $img.parentNode.parentNode.childNodes[1].textContent;  
-    document.querySelector("body").innerHTML += "<div class='lightbox'><figure><img src="+sLightboxSrc+"></img><figcaption>"+sCaption+"</figcaption></figure></div>";
+    document.querySelector("body").innerHTML += "<div class='lightbox'><figure><img src="+sLightboxSrc+"><div class='loader'></div><figcaption>"+sCaption+"</figcaption></figure></div>";
     
     $lightbox = document.querySelector(".lightbox");
-    $lightbox.addEventListener("click", function() {
+    document.querySelector(".lightbox img").addEventListener("load", function() {
+        document.querySelector(".lightbox figure").removeChild(document.querySelector("div.loader"));
+    });
+
+    const fRemoveLightBox = function() {
         $lightbox.parentNode.removeChild($lightbox);
         Array.from( document.querySelectorAll( "figure img" ) ).forEach( function( $elt ) {
-            $elt.addEventListener( "click", fShowLightbox );
+            $elt.addEventListener( "click", fHandleLightbox );
         } );
-    });
+    }
+    $lightbox.addEventListener("click", fRemoveLightBox);
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+            fRemoveLightBox();
+    }
+};
 }
 
 
