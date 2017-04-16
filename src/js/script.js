91e911c3d1a7600f727c726b9880e708add2b425
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-04-16 15:36:40 +0200

    Fixed lightbox caption

--- src/js/script.js
+++ src/js/script.js
@@ -2,26 +2,23 @@ const fHandleNavbar = function() {
     document.querySelector("header nav ul").classList.toggle("hidden");
 };
 
-const fRemoveLightbox = function(oEvent) {
-    let $lightbox = oEvent.currentTarget;
-    $lightbox.parentNode.removeChild($lightbox);
-};
-
 const fShowLightbox = function(oEvent) {
     let sCaption, $img, sImgSrc, sLightboxSrc, $lightbox;
+    oEvent.preventDefault();
     $img = oEvent.currentTarget;
 
     sImgSrc = $img.getAttribute("src");
     sLightboxSrc = sImgSrc.substring(0, sImgSrc.indexOf("."))+ "-full.jpg";
-    //sCaption = $img.querySelector("figcaption").textContent;
+    sCaption = $img.parentNode.parentNode.childNodes[1].textContent;  
     document.querySelector("body").innerHTML += "<div class='lightbox'><figure><img src="+sLightboxSrc+"></img><figcaption>"+sCaption+"</figcaption></figure></div>";
+    
     $lightbox = document.querySelector(".lightbox");
     $lightbox.addEventListener("click", function(oEvent) {
         let $lightbox = oEvent.currentTarget;
         $lightbox.parentNode.removeChild($lightbox);
         Array.from( document.querySelectorAll( "figure img" ) ).forEach( function( $elt ) {
-        $elt.addEventListener( "click", fShowLightbox );
-    } );
+            $elt.addEventListener( "click", fShowLightbox );
+        } );
     });
 }
 
