1db336630350078c06f69bd84f821dc6c0757b7e
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-09 12:21:20 +0200

    Started work on galerie2

--- src/js/_galleryTab.js
+++ src/js/_galleryTab.js
@@ -2,17 +2,17 @@ const galleries = {
     "gallery1": {
         "name": "gallery1",
         "figures": [
-            ["assets/img/galerie1-full.jpg", "assets/img/galerie1.jpg", "First-aid station. Mutter Ridge, Nui Cay Tri", "First-aid station. Mutter Ridge, Nui Cay Tri"],
-            ["assets/img/galerie2-full.jpg", "assets/img/galerie2.jpg", "Helicopter evacuation of the wounded, near hill 400", "Helicopter evacuation of the wounded, near hill 400"],
-            ["assets/img/galerie3-full.jpg", "assets/img/galerie3.jpg", "a mortar-equipped marine wades through another of the rivers", "Within feet of the DMZ and at constant risk of ambush, a mortar-equipped marine wades through another of the rivers and steams that combined with the hot, jungled and mountainous terrain to make maneuvering on this operation such tough going"],
-            ["assets/img/galerie4-full.jpg", "assets/img/galerie4.jpg", "At First-Aid Center During Operation Prairie", "At First-Aid Center During Operation Prairie"],
-            ["assets/img/galerie5-full.jpg", "assets/img/galerie5.jpg", "Casualty of the ultimately successful assault on Hill 484", "Casualty of the ultimately successful assault on Hill 484"],
-            ["assets/img/galerie6-full.jpg", "assets/img/galerie6.jpg", "Ammunition airlift during the relief of Khe Sanh", "Ammunition airlift during the relief of Khe Sanh"],
-            ["assets/img/galerie7-full.jpg", "assets/img/galerie7.jpg", "Lance Cpl. James C. Farley manning helicopter machine gun of Yankee Papa 13", "Crew chief Lance Cpl. James C. Farley manning helicopter machine gun of Yankee Papa 13"],
-            ["assets/img/galerie8-full.jpg", "assets/img/galerie8.jpg", "Helicopter crew chief shouting to crew as wounded pilot lay dying, Vietnam", "Helicopter crew chief James C. Farley shouting to crew as wounded pilot Lt. James E. Magel and Billy Owens lay dying, Vietnam"],
-            ["assets/img/galerie9-full.jpg", "assets/img/galerie9.jpg", "James Farley checking on a wounded crewmate while pilot lies dying at his feet", "Helicopter crew chief James C. Farley (R) checking on a wounded crewmate while wounded pilot, Lt. James E. Magel, lies dying at his feet"],
-            ["assets/img/galerie10-full.jpg", "assets/img/galerie10.jpg", "Lance Cpl. James Farley putting on helmet aboard Yankee Papa 13", "Lance Cpl. James Farley, helicopter crew chief, putting on helmet aboard Yankee Papa 13"],
-            ["assets/img/galerie11-full.jpg", "assets/img/galerie11.jpg", "Wayne Holien (PFC - Marine copter gunner in Vietnam)", "Wayne Holien (PFC - Marine copter gunner in Vietnam)"]
+            ["assets/img/gallery1/galerie1_1-full.jpg", "assets/img/gallery1/galerie1_1.jpg", "First-aid station. Mutter Ridge, Nui Cay Tri", "First-aid station. Mutter Ridge, Nui Cay Tri"],
+            ["assets/img/gallery1/galerie1_2-full.jpg", "assets/img/gallery1/galerie1_2.jpg", "Helicopter evacuation of the wounded, near hill 400", "Helicopter evacuation of the wounded, near hill 400"],
+            ["assets/img/gallery1/galerie1_3-full.jpg", "assets/img/gallery1/galerie1_3.jpg", "a mortar-equipped marine wades through another of the rivers", "Within feet of the DMZ and at constant risk of ambush, a mortar-equipped marine wades through another of the rivers and steams that combined with the hot, jungled and mountainous terrain to make maneuvering on this operation such tough going"],
+            ["assets/img/gallery1/galerie1_4-full.jpg", "assets/img/gallery1/galerie1_4.jpg", "At First-Aid Center During Operation Prairie", "At First-Aid Center During Operation Prairie"],
+            ["assets/img/gallery1/galerie1_5-full.jpg", "assets/img/gallery1/galerie1_5.jpg", "Casualty of the ultimately successful assault on Hill 484", "Casualty of the ultimately successful assault on Hill 484"],
+            ["assets/img/gallery1/galerie1_6-full.jpg", "assets/img/gallery1/galerie1_6.jpg", "Ammunition airlift during the relief of Khe Sanh", "Ammunition airlift during the relief of Khe Sanh"],
+            ["assets/img/gallery1/galerie1_7-full.jpg", "assets/img/gallery1/galerie1_7.jpg", "Lance Cpl. James C. Farley manning helicopter machine gun of Yankee Papa 13", "Crew chief Lance Cpl. James C. Farley manning helicopter machine gun of Yankee Papa 13"],
+            ["assets/img/gallery1/galerie1_8-full.jpg", "assets/img/gallery1/galerie1_8.jpg", "Helicopter crew chief shouting to crew as wounded pilot lay dying, Vietnam", "Helicopter crew chief James C. Farley shouting to crew as wounded pilot Lt. James E. Magel and Billy Owens lay dying, Vietnam"],
+            ["assets/img/gallery1/galerie1_9-full.jpg", "assets/img/gallery1/galerie1_9.jpg", "James Farley checking on a wounded crewmate while pilot lies dying at his feet", "Helicopter crew chief James C. Farley (R) checking on a wounded crewmate while wounded pilot, Lt. James E. Magel, lies dying at his feet"],
+            ["assets/img/gallery1/galerie1_10-full.jpg", "assets/img/gallery1/galerie1_10.jpg", "Lance Cpl. James Farley putting on helmet aboard Yankee Papa 13", "Lance Cpl. James Farley, helicopter crew chief, putting on helmet aboard Yankee Papa 13"],
+            ["assets/img/gallery1/galerie1_11-full.jpg", "assets/img/gallery1/galerie1_11.jpg", "Wayne Holien (PFC - Marine copter gunner in Vietnam)", "Wayne Holien (PFC - Marine copter gunner in Vietnam)"]
         ]
     }
 }
